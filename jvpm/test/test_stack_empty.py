4e545df5919061b6123d022a42c32c7928435652
johnhshapiro <jshapir4@msudenver.edu>
2019-05-07 09:27:35 -0600

    re-added test_stack_empty which still worked after new structure

--- jvpm/test/test_stack_empty.py
+++ jvpm/test/test_stack_empty.py
@@ -3,8 +3,8 @@ from unittest import TestCase
 from jvpm.jvm_stack import *
 class TestStackEmpty(TestCase):
     """tests the case where the stack is empty"""
-    # def test_stack_empty(self):
-    #     """tests the case where the stack is empty"""
-    #     jvm_stack = JvmStack()
-    #     with self.assertRaises(EmptyStackError):
-    #         jvm_stack.pop_op()
+    def test_stack_empty(self):
+        """tests the case where the stack is empty"""
+        jvm_stack = JvmStack()
+        with self.assertRaises(EmptyStackError):
+            jvm_stack.pop_op()
