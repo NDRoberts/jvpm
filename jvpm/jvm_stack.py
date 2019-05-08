0ef22d74a37bccd33c3d05365af93a4890c99a54
Kelly <ktruji25@msudenver.edu>
2019-05-08 14:46:50 -0600

    pydocs for jvm_stack.py

--- jvpm/jvm_stack.py
+++ jvpm/jvm_stack.py
@@ -1,43 +1,42 @@
-"""This class implements the operand stack for the jvm"""
+"""This class implements the operand stack for the jvm."""
 
 
 class JvmStack:
-    """JvmStack"""
+    """JvmStack."""
 
     def __init__(self):
-        """Constructor. Initializes the stack to be empty"""
+        """Constructor. Initializes the stack to be empty."""
         self.stack = []
         self.local_array = [0, 1, 2, 3]
 
+    """Push_strategy is a function that determines how the pushing is done.
+    This is necessary because when pushing longs and doubles, two pushes are required.
+    """
     def push_op(self, obj, push_strategy=lambda stack, obj: stack.append(obj)):
-        """pushes one operand onto the top of the stack.
-push_strategy is a function that determines how the pushing is done.
-This is necessary because when pushing longs and doubles, two pushes are required.
-"""
+        """Pushes one operand onto the top of the stack."""
         push_strategy(self.stack, obj)
 
+    """If the stack is empty, then this method raises an EmptyStackError."""
+
     def pop_helper(self):
-        """Pops one object off of the stack and then returns it
-If the stack is empty, then this method raises an EmptyStackError
-"""
+        """Pops one object off of the stack and then returns it."""
         if self.stack:
             return self.stack.pop()
         raise EmptyStackError("Can't pop from an empty stack")
 
     def pop_op(self, pop_strategy=lambda stack: stack.pop_helper()):
-        """This method modifies the original pop_op.
-         It enables the use of different popping strategies
-         """
+        """Modify the original pop_op. It enables the use of different popping strategies."""
         return pop_strategy(self)
 
+    """Return the top element of the stack without popping it.
+        This doesn't seem useful now,
+        but it is one of the standard stack operations that might be useful later
+        Since longs and doubles take up two spaces on the stack, I will push None on top of such values.
+        However, to remain consistent with past usage, this method will not return the None.
+        Instead, it will return the actual long or double value.
+        """
     def peek(self):
-        """returns the top element of the stack without popping it.
-This doesn't seem useful now,
-but it is one of the standard stack operations that might be useful later
-Since longs and doubles take up two spaces on the stack, I will push None on top of such values.
-However, to remain consistent with past usage, this method will not return the None.
-Instead, it will return the actual long or double value.
-"""
+        """Will not return the None.Instead, it will return the actual long or double value."""
         index = -1
         while self.stack[index] is None:
             index -= 1
@@ -45,16 +44,16 @@ Instead, it will return the actual long or double value.
 
 
 class EmptyStackError(Exception):
-    """error raised when there is an empty stack"""
+    """Error raised when there is an empty stack."""
 
 
 def push_twice(stack, obj):
-    """push method to handle long and fp types"""
+    """Push method to handle long and fp types."""
     stack.append(obj)
     stack.append(None)
 
 
 def pop_twice(stack):
-    """pop method to handle long and fp types"""
+    """Pop method to handle long and fp types."""
     stack.pop_helper()
     return stack.pop_helper()
